a194b83d5382bb9d18265ee4a4919481942f7f7a
binamracode <binamraprogrammer@gmail.com>
2021-01-31 21:04:11 +0545

    Added dropdown

--- js/script.js
+++ js/script.js
@@ -21,3 +21,23 @@ document.addEventListener("DOMContentLoaded", () => {
       applyTheme(this.value);
     });
 });
+
+function openDropdown(id) {
+  console.log(id);
+  document.getElementById(id).classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    console.log("hmm");
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
